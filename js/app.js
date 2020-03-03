df11011f1af2b6eeed8aa0f11026564a55c2bac2
Jacob Sima <simajacob2011@gmail.com>
2020-03-03 20:16:06 +0200

    home page completed

--- js/app.js
+++ js/app.js
@@ -3,12 +3,44 @@
       // Custom JS & jQuery here
       // sidenav
        $('.sidenav').sidenav();
+       $('.carousel.carousel-slider').carousel({ fullWidth: true });
     
 
     });
 
 const nav = document.querySelector('nav')  
 const li = document.querySelector('.active-link-bottom') 
+const staticIcon = document.querySelector('.static-arrow')
+const atelierClick = document.querySelector('.atelier-right a')
+const coordinationClick = document.querySelector('.coordination-right a')
+const missionsClick = document.querySelector('.missions-right a')
+const activityDivs = document.querySelectorAll('.activity-right div')
+
+// function Activity
+const activity = e =>{
+  if(e.target.parentElement.classList.contains('atelier-right')){
+    document.querySelector('.activity-right .atelier').style.display='block'
+    document.querySelector('.activity-right .coordination').style.display='none'
+    document.querySelector('.activity-right .missions-de-terrain').style.display='none'
+  }
+  if(e.target.parentElement.classList.contains('coordination-right')){
+    document.querySelector('.activity-right .atelier').style.display='none'
+    document.querySelector('.activity-right .coordination').style.display='block'
+    document.querySelector('.activity-right .missions-de-terrain').style.display='none'
+  }
+  if(e.target.parentElement.classList.contains('missions-right')){
+    document.querySelector('.activity-right .atelier').style.display='none'
+    document.querySelector('.activity-right .coordination').style.display='none'
+    document.querySelector('.activity-right .missions-de-terrain').style.display='block'
+  }
+}
+
+
+
+atelierClick.addEventListener('click',activity)
+coordinationClick.addEventListener('click',activity)
+missionsClick.addEventListener('click',activity)
+
 
 window.addEventListener("scroll",()=>{
   const fixed = document.querySelector('#fixed')
@@ -20,12 +52,15 @@ window.addEventListener("scroll",()=>{
      nav.style.transition ='0.4s'
      li.classList.remove('active-link-bottom')
      li.classList.add('active-link-left')
+     staticIcon.style.display = 'block'
+
   }else{
     nav.classList.add('transparent')
     nav.style.height = '70px'
-    nav.style.transition ='0.4s'
+    nav.style.transition ='0.6s'
     li.classList.remove('active-link-left')
     li.classList.add('active-link-bottom')
+    staticIcon.style.display = 'none'
    
   }
 })
